34ccf70af2235ef30326bf4dbb383abcebd65431
ecyriaque <101125367+ecyriaque@users.noreply.github.com>
2024-09-22 15:09:15 +0200

    Update person.tsx

---
+++ src/app/models/person.tsx
@@ -0,0 +1,18 @@
+// models/person.ts
+import { Animal } from "./animal";
+
+export class Person {
+  id: number;
+  firstName: string;
+  lastName: string;
+  animals: Animal[];
+
+  constructor(data: any) {
+    this.id = data.id;
+    this.firstName = data.firstName;
+    this.lastName = data.lastName;
+    this.animals = data.animals
+      ? data.animals.map((animal: any) => new Animal(animal))
+      : [];
+  }
+}
