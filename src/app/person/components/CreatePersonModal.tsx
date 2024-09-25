99bf7190acefc8a0e49052efde5e765151e8d579
ecyriaque <101125367+ecyriaque@users.noreply.github.com>
2024-09-26 01:56:59 +0200

    style: Round modal corners and adjust mobile responsiveness

--- src/app/person/components/CreatePersonModal.tsx
+++ src/app/person/components/CreatePersonModal.tsx
@@ -12,11 +12,17 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: "90%",
+  maxWidth: 280,
   bgcolor: "background.paper",
   border: "2px solid #000",
+  borderRadius: 8,
   boxShadow: 24,
   p: 4,
+  "@media (max-width:600px)": {
+    width: "90%",
+    margin: "0 16px",
+  },
 };
 
 interface CreatePersonModalProps {
